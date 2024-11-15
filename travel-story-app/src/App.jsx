4739cbf80a60f6f8f7142731c79267c967cb9799
Natanel Gabay <natanelg1@gmail.com>
2024-11-15 13:38:41 +0200

    Implement routing with react-router-dom and add Home, Login, and SignUp pages

--- travel-story-app/src/App.jsx
+++ travel-story-app/src/App.jsx
@@ -1,7 +1,20 @@
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SignUp from './pages/Auth/Signup';
+import Login from './pages/Auth/Login';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
