235bff380b6dc752f9dc5c607697cd315d246a8e
Natanel Gabay <natanelg1@gmail.com>
2024-12-09 23:00:39 +0200

    Add DateSelector and ImageSelector components, integrate react-day-picker for date selection, and enhance AddEditTravelStory layout

--- client/travel-story-app/src/pages/Home/AddEditTravelStory.jsx
+++ client/travel-story-app/src/pages/Home/AddEditTravelStory.jsx
@@ -1,10 +1,93 @@
+import { MdAdd, MdClose, MdUpdate, MdDeleteOutline } from "react-icons/md";
+import PropTypes from "prop-types";
+import DateSelector from "../../components/Input/DateSelector";
+import { useState } from "react";
 
-const AddEditTravelStory = ({storyInfo, type, onClose, getAllTravelStories}) => {
+const AddEditTravelStory = ({
+  storyInfo,
+  type,
+  onClose,
+  getAllTravelStories,
+}) => {
+  const [title, setTitle] = useState("");
+  const [storyImg, setStoryImg] = useState(null);
+  const [story, setStory] = useState("");
+  const [visitedLocation, setVisitedLocation] = useState([]);
+  const [visitedDate, setVisitedDate] = useState(null);
+
+  const handleAddOrUpdateClick = () => {};
+
   return (
     <div>
-        AddEditTravelStory
-    </div>
-  )
-}
+      <div className="flex items-center justify-between">
+        <h5 className="text-xl font-medium text-slate-700">
+          {type === "add" ? "Add Travel Story" : "Edit Travel Story"}
+        </h5>
 
-export default AddEditTravelStory
+        <div>
+          <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
+            {type === "add" ? (
+              <button className="btn-small" onClick={handleAddOrUpdateClick}>
+                <MdAdd className="text-lg" /> Add a story
+              </button>
+            ) : (
+              <>
+                <button className="btn-small" onClick={handleAddOrUpdateClick}>
+                  <MdUpdate className="text-lg" /> Update story
+                </button>
+
+                <button className="btn-small btn-delete" onClick={onClose}>
+                  <MdDeleteOutline className="text-lg" /> Delete story
+                </button>
+              </>
+            )}
+
+            <button className="" onClick={onClose}>
+              <MdClose className="text-xl text-slate-400 hover:bg-slate-200/50 transition-all duration-300 ease-in-out" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex-1 flex flex-col gap-2 pt-4">
+          <label className="input-label">Title</label>
+          <input
+            type="text"
+            className="text-2xl text-slate-950 outline-none"
+            placeholder="A day in Washington DC"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+
+          <div className="my-3">
+            <DateSelector date={visitedDate} setDate={setVisitedDate} />
+          </div>
+
+          <ImageSelector image={storyImg} setImage={setStoryImg} />
+
+          <div className="flex flex-col gap-2 mt-4">
+            <label className="input-label">Story</label>
+            <textarea
+              type="text"
+              className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+              placeholder="Your story here..."
+              rows={10}
+              value={story}
+              onChange={({ target }) => setStory(target.value)}
+            ></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddEditTravelStory;
+
+AddEditTravelStory.propTypes = {
+  storyInfo: PropTypes.object,
+  type: PropTypes.string,
+  onClose: PropTypes.func,
+  getAllTravelStories: PropTypes.func,
+};
